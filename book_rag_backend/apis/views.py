db4f24cef731bb0c58c3d6734674f781c181880d
Jackson Socolofsky <jtsoco@gmail.com>
2026-02-13 17:42:18 +0900

    commenting things out for the commit -m

--- book_rag_backend/apis/views.py
+++ book_rag_backend/apis/views.py
@@ -15,3 +15,13 @@ class BookAPIView(generics.RetrieveAPIView):
     # this method will retrieve the book based on the open library key passed in the url
     # if the book is not found, it will attempt to fetch the data from open library, store it in the database, and then return it
     # failing that, it will return a 404 error
+
+class OpenLibraryFetchIfNotFoundMixin:
+    # this will be used for author and book views
+    def get_object(self):
+
+        pk = self.kwargs.get(self.lookup_field)
+        model = self.get_queryset().model
+        # try:
+        #     return model.objects.get(**{self.lookup_field: pk})
+        # except model.DoesNotExist:
