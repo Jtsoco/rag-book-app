53e08336fc17d2b13fc6f2743b8ce5e22d20ba97
Jackson Socolofsky <jtsoco@gmail.com>
2026-02-23 17:19:33 +0900

    wrote code for chatbot service

--- book_rag_backend/chatbot/services/chatbot.py
+++ book_rag_backend/chatbot/services/chatbot.py
@@ -1,13 +1,64 @@
 import json
-from .verify_books import verify_all
+# from .verify_books import verify_all
+from openai import OpenAI
+import os
+import dotenv
+from .context import get_context, get_schema
+# will use response api
 
-def ask(query):
-    response = get_book_recommendation(query)
+
+def _get_client(api_key=None):
+    if api_key is None:
+        dotenv.load_dotenv()
+        api_key = os.getenv("OPENAI_API_KEY")
+    return OpenAI(api_key)
+
+
+def ask(query, user=None, client=None):
+    if client is None:
+        client = _get_client()
+
+    user_data = get_user_data(user)
+    formatted_query = _format_query(query, user_data)
+    response = client.responses.create(**formatted_query)
+
+    # get messages for gpt,
+        # first initial context message explaing what it does
+            # role: developer content: the message
+        # then build user profile, send it
+        # final message specifying role: user content: query
+    # response = get_book_recommendation(query, user_data)
     processed_response = process_reply(response)
     return processed_response
 
-def get_book_recommendation(query):
-    pass
+
+def get_user_data(user):
+    if not user:
+        return None
+    else:
+        return None
+        # not implemented yet
+    # get user data from db, including bookshelf data and ratings, and return it in a format that can be sent to gpt
+
+
+def _format_query(query, user_data, tools=[], model='gpt-5-nano', reasoning={"effort": "low"}):
+    input = query
+    context = get_context()
+    schema = get_schema()
+    if user_data:
+        # convert user_data to string, then enter it
+        input += f"\nUser Data: {user_data}"
+
+    formatted = {
+        'instructions': context,
+        'input': input,
+        'tools': tools,
+        'text': {schema},
+        'model': model,
+        "reasoning": reasoning
+    }
+    return formatted
+
 
 def process_reply(reply):
     try:
@@ -17,6 +68,7 @@ def process_reply(reply):
         if (books is None) or (books == []):
             verified_books = []
         else:
+            # later, use method to verify against my db or external api
             return {'books': verified_books, 'assistant_reply': assistant_reply}
     except json.JSONDecodeError:
         return {'books': [], 'assistant_reply': "Sorry, I couldn't understand the response from the recommendation system."}
---
+++ book_rag_backend/chatbot/services/context.py
@@ -0,0 +1,38 @@
+def get_context():
+    # may become dynamic later, static for now but in getter
+    context = 'You are a helpful assistant that helps users find books based on their preferences. You have access to a database of books and can provide recommendations based on user input. When recommending books, provide a brief summary and the author\'s name. If you cannot find a suitable book, politely inform the user that you have no recommendations at this time. If they want more info on a book, please provide it. It is not necessary to provide book recommendations if all they want is more info on the books.'
+    return context
+
+def get_schema():
+    # may change to be more dynamic in the future
+
+    books_parameters = {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "title": {"type": "string"},
+                    "author": {"type": "string"},
+                    "description": {"type": "string"},
+                    "genre": {"type": "string"},
+                    "comment_on_book": {"type": "string"},
+                },
+                "required": ["title", "author", "comment_on_book"],
+            }
+        }
+
+    format = {
+            "type": "json_schema",
+
+            "name": "BookRecommendations",
+            "schema": {
+
+                "type": "object",
+                "properties": {
+                    "books": books_parameters,
+                    "assistant_reply": {"type": "string"},
+                },
+            },
+
+        }
+    return format
