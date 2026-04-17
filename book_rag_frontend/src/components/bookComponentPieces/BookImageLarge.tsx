a287a676d1a7f0f20bbe82fa0065289eaae41057
Jackson Socolofsky <jtsoco@gmail.com>
2026-04-17 19:35:48 +0900

    image component making

---
+++ book_rag_frontend/src/components/bookComponentPieces/BookImageLarge.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+export interface BookImageLargeProps {
+  url: string;
+  altText: string;
+}
+
+const BookImageLarge = (props: BookImageLargeProps) => {
+  const [imageError, setImageError] = useState(false);
+
+    const { url, altText } = props;
+
+    const getImageElement = () => {
+      if (imageError) {
+        return (
+          <div className="w-full h-full bg-gray-400 flex items-center justify-center p-4">
+            <div className="text-center text-white bg-black p-4 rounded" >
+              <h3 className="text-sm font-bold">{altText}</h3>
+
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <img
+            src={url}
+            alt={`${altText}`}
+            className="w-full h-full object-cover"
+            onError={() => setImageError(true)}
+          />
+        );
+      }
+    };
+
+    return()
+  }
