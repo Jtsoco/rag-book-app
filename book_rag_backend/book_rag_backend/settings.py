424a8a2d6d61e2a7333dcccd86ffc75851bcd757
Jackson Socolofsky <jtsoco@gmail.com>
2026-07-21 13:14:19 +0900

    cors setup and temporary allow any

--- book_rag_backend/book_rag_backend/settings.py
+++ book_rag_backend/book_rag_backend/settings.py
@@ -53,11 +53,14 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    # i added
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,12 +129,24 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',],
+        # 'rest_framework.permissions.IsAuthenticated',
+        #  'AllowAny' allows unrestricted access to the API endpoints
+        'rest_framework.permissions.AllowAny',
+        ],
 
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication"]
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React default
+    "http://127.0.0.1:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 # django-allauth configurations
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
